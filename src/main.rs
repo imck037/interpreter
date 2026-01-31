2098dc7754610524f0b16dd8cd557644865e30e4
imck037 <chandradhar.2003@gmail.com>
2026-01-31 19:10:40 +0530

    lexer is implemeted

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+enum Token {
+    Atom(char),
+    Operator(char),
+    Eof,
+}
+
+struct Lexer {
+    tokens: Vec<Token>,
+}
+
+impl Lexer {
+    fn new(input: &str) -> Lexer {
+        let mut tokens = input
+            .chars()
+            .filter(|item| !item.is_ascii_whitespace())
+            .map(|c| match c {
+                '0'..'9' | 'a'..='z' | 'A'..='Z' => Token::Atom(c),
+                _ => Token::Operator(c),
+            })
+            .collect::<Vec<_>>();
+        Lexer { tokens }
+    }
+}
+
+fn parse_expression(lexer: &Lexer) -> Expression {
+    unimplemented!();
+}
+
+enum Expression {
+    Atom(char),
+    Operation(char, Vec<Expression>),
+}
+
+impl Expression {
+    fn from_input(input: &str) -> Expression {
+        let mut lexer = Lexer::new(input);
+        parse_expression(&lexer)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
