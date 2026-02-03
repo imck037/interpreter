f339b7f792808ce710dbcdd88de73f100792171c
imck037 <chandradhar.2003@gmail.com>
2026-02-03 20:11:40 +0530

    interpreter is done for single digit and single character variable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-#[derive(Clone, Copy)]
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Token {
     Operand(char),
     Operator(char),
@@ -32,8 +37,44 @@ impl Lexer {
     }
 }
 
-fn parse_expression(lexer: &Lexer) -> Expression {
-    unimplemented!();
+fn parse_expression(lexer: &mut Lexer, min_bp: f32) -> Expression {
+    let mut lhs = match lexer.next() {
+        Token::Operand(it) => Expression::Operand(it),
+        Token::Operator('(') => {
+            let lhs = parse_expression(lexer, 0.0);
+            assert_eq!(lexer.next(), Token::Operator(')'));
+            lhs
+        }
+        t => panic!("bad token: {:?}", t),
+    };
+    loop {
+        let op = match lexer.peek() {
+            Token::Eof => break,
+            Token::Operator(')') => break,
+            Token::Operator(op) => op,
+            t => panic!("bad token: {:?}", t),
+        };
+
+        let (l_bp, r_bp) = infix_binding_power(op);
+        if l_bp < min_bp {
+            break;
+        }
+        lexer.next();
+        let rhs = parse_expression(lexer, r_bp);
+        lhs = Expression::Operation(op, vec![lhs, rhs]);
+    }
+    lhs
+}
+
+fn infix_binding_power(op: char) -> (f32, f32) {
+    match op {
+        '=' => (0.2, 0.1),
+        '+' | '-' => (1.0, 1.1),
+        '*' | '/' => (2.0, 2.1),
+        '^' | '√' => (3.1, 3.0),
+        '.' => (4.0, 4.1),
+        _ => panic!("bad op: {:?}", op),
+    }
 }
 
 enum Expression {
@@ -44,10 +85,77 @@ enum Expression {
 impl Expression {
     fn from_input(input: &str) -> Expression {
         let mut lexer = Lexer::new(input);
-        parse_expression(&lexer)
+        parse_expression(&mut lexer, 0.0)
+    }
+    #[allow(unused)]
+    fn is_asign(&self) -> Option<(char, &Expression)> {
+        match self {
+            Expression::Operand(_) => return None,
+            Expression::Operation(c, operands) => {
+                if *c == '=' {
+                    let var_name = match operands.first().unwrap() {
+                        Expression::Operand(c) => {
+                            if *c >= 'a' && *c <= 'z' || *c >= 'A' && *c <= 'Z' {
+                                *c
+                            } else {
+                                panic!("Not a variable name: {}", c)
+                            }
+                        }
+                        _ => unreachable!(),
+                    };
+                    return Some((var_name, operands.last().unwrap()));
+                }
+                return None;
+            }
+        }
+    }
+    #[allow(unused)]
+    fn eval(&self, variables: &HashMap<char, f32>) -> f32 {
+        match self {
+            Expression::Operand(c) => match c {
+                '0'..='9' => return c.to_digit(10).unwrap() as f32,
+                'a'..='z' | 'A'..='Z' => *variables
+                    .get(c)
+                    .expect(&format!("Undefined variable {}", c)),
+                _ => unreachable!(),
+            },
+            Expression::Operation(operator, operands) => {
+                let lhs = operands.first().unwrap().eval(variables);
+                let rhs = operands.last().unwrap().eval(variables);
+                match operator {
+                    '+' => return lhs + rhs,
+                    '-' => return lhs - rhs,
+                    '*' => return lhs * rhs,
+                    '/' => return lhs / rhs,
+                    '^' => return lhs.powf(rhs),
+                    '√' => return lhs.powf(1.0 / (rhs)),
+                    op => panic!("Bad operator: {}", op),
+                }
+            }
+        }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut variables: HashMap<char, f32> = HashMap::new();
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+        let input = {
+            let mut buf = String::new();
+            std::io::stdin().read_line(&mut buf).unwrap();
+            buf
+        };
+        if input.trim() == "exit" {
+            break;
+        }
+        let expr = Expression::from_input(&input);
+        if let Some((var_name, lhs)) = expr.is_asign() {
+            let value = lhs.eval(&variables);
+            variables.insert(var_name, value);
+            continue;
+        }
+        let value = expr.eval(&variables);
+        println!("{}", value);
+    }
 }
